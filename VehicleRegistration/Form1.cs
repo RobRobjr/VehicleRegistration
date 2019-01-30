77c672c319c14a2a8adbb88752659940f97266bc
Robert Robinson <robinson6113@student.cptc.edu>
2019-01-30 14:22:42 -0800

    Added buttons for update, delete and view

--- VehicleRegistration/Form1.cs
+++ VehicleRegistration/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace VehicleRegistration
 {
-    public partial class Form1 : Form
+    public partial class FrmVehicleRegistration : Form
     {
-        public Form1()
+        public FrmVehicleRegistration()
         {
             InitializeComponent();
         }
